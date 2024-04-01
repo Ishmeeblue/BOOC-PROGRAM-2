048e490262bb4c459291d23bcdb145b3f1d22d75
Ishmeeblue <blue14colline@gmail.com>
2024-04-01 18:50:12 +0800

    Initial commit

---
+++ PROGRAM 2 BOOC/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PROGRAM_2_BOOC
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number = 1;
+            while (number <= 100)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("Hello Goodbye");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+                number++;
+            }
+        }
+    }
+}
